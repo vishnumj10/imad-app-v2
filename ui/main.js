9f666e4e7f27d148f5070f3c8d0a3fd2e2741825
vishnumj10 <vishnumj10@gmail.com>
2017-02-04 21:27:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var ob=document.getElementById('one');
+function moveRight(){
+    marginLeft=marginLeft+1;
+    ob.style.marginLeft=marginLeft+'px';
+}
+ob.onclick=function() {
+    var interval=setInterval(moveRight,50);
+};
