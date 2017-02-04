f699d999c19e5fd545c3a4df9bd768dbefcefea1
vishnumj10 <vishnumj10@gmail.com>
2017-02-04 20:41:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/arttwo', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'arttwo.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
