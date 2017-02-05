79c6e7b7df723399514e9940fdb762ed3b4ad123
vishnumj10 <vishnumj10@gmail.com>
2017-02-05 09:49:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
-var ob=document.getElementById('counter');
+var button=document.getElementById('counter');
 var counter=0;
-ob.onclick=function() {
+button.onclick=function() {
    counter=counter+1;
    var span=document.getElementById('count');
    span.innerHTML=counter.toString();
