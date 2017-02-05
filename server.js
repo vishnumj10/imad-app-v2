388d33824951f7ef6571fcdae8e153c45334298b
vishnumj10 <vishnumj10@gmail.com>
2017-02-05 08:22:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 app.get('/artone', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'artone.html'));
 });
-stdid=1;
+var stdid=1;
 app.get('/reg', function (req, res) {
   if(stdid==10)
     res.sent('registration over');
