23734080e4e0e2f3746faf2d416d3460eb4e7058
vishnumj10 <vishnumj10@gmail.com>
2017-02-05 08:23:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,12 +14,7 @@ app.get('/artone', function (req, res) {
 });
 var stdid=1;
 app.get('/reg', function (req, res) {
-  if(stdid==10)
-    res.sent('registration over');
-  else
-    res.sent('registation sucess . ID is');
     res.sent(stdid.tostring());
-    stdid=stdid+1;
 });
 
 app.get('/arttwo', function (req, res) {
