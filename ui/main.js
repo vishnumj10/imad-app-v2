72f8e387dc24464014d0724d122876691fbf99c5
vishnumj10 <vishnumj10@gmail.com>
2017-02-05 09:43:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var ob=document.getElementById('btn');
 var x=0;
 ob.onclick=function() {
-   x=x+;
+   x=x+1;
    var span=document.getElementById('val');
    span.innerHTML=x.toString();
 };
